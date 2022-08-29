d1d2961b84f48411740cf923a92273d5d5821aa4
marianovungel <vungem88@gmail.com>
2022-08-29 13:42:02 -0300

    text voice

---
+++ script.js
@@ -0,0 +1,35 @@
+var txtInput = document.querySelector('#txtInput')
+var voiceList = document.querySelector('#voiceList')
+var btnSpeak = document.querySelector('#btnSpeak')
+var synth = window.speechSynthesis;
+var voices = []
+
+NewVoices()
+if(speechSynthesis !== undefined){
+    speechSynthesis.onvoiceschanged = NewVoices;
+}
+
+btnSpeak.addEventListener('click', ()=>{
+    var toSpeak = new SpeechSynthesisUtterance(txtInput.value)
+    var selectedVoiceName = voiceList.selectedOptions[0].getAttibute('data-name')
+    voices.forEach((voice)=>{
+        if(voice.name === selectedVoiceName){
+            toSpeak.voice = voice
+        }
+    })
+    synth.speak(toSpeak);
+})
+
+function NewVoices(){
+    voices = synth.getVoices()
+    var selectedIndex = voiceList.selectedIndex < 0 ? 0 : voiceList.selectedIndex
+    voiceList.innerHTML = ''
+    voices.forEach((voice)=>{
+        var listItem = document.createElement('option')
+        listItem.textContent = voice.name
+        listItem.setAttribute('data-lang', voice.lang)
+        listItem.setAttribute('data-name', voice.name)
+        voiceList.appendChild(listItem)
+    })
+    voiceList.selectedIndex = selectedIndex;
+}
